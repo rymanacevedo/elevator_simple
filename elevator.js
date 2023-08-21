bafe91d6a37a8587b59b6037045399bc43151537
Ryan Acevedo <rymanacevedo@gmail.com>
2023-08-21 12:52:37 -0600

    return a result

--- elevator.js
+++ elevator.js
@@ -1,11 +1,12 @@
 class Elevator {
+  FLOOR_TIME = 10;
   constructor(startFloor) {
     this.currentFloor = startFloor;
     this.travelTime = 0;
   }
 
   goTo(floor) {
-    this.travelTime += (floor - this.currentFloor) * 10;
+    this.travelTime += (floor - this.currentFloor) * this.FLOOR_TIME;
     this.currentFloor = floor;
   }
 
@@ -15,10 +16,16 @@ const trip = (start, ...floors) => {
   for (const floor of floors) {
     elevator.goTo(floor);
   }
+
+  return {
+    time: elevator.travelTime
+  }
 }
 
 
 const input = prompt('Enter the elevator start and floors to visit seperated by commas (e.g. 12,2,9,1,32):');
 
 const floors = input.split(',').map(Number);
-const startFllor = floor[0];
+const startFloor = floor[0];
+const result = trip(startFloor, ...floors.slice(1));
+
