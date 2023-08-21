4b5c5e65a63534806f445b7589ba668a0a38bc46
Ryan Acevedo <rymanacevedo@gmail.com>
2023-08-21 12:49:13 -0600

    map over the floor prompt

--- elevator.js
+++ elevator.js
@@ -13,3 +13,6 @@ class Elevator {
 
 
 const input = prompt('Enter the elevator start and floors to visit seperated by commas (e.g. 12,2,9,1,32):');
+
+const floors = input.split(',').map(Number);
+const startFllor = floor[0];
